dad83a3120902af0bf474d89f6c86158b6096205
unknown <joethan159@gmail.com>
2026-09-22 14:32:53 +0100

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import person from "./assets/class 1.jpg"
-import personimg from "./assets/class 2.jpg"
-import own from "./assets/class 3.jpg"
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
